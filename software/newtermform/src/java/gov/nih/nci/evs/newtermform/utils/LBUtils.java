933da400ba4db443231a4092c23377b9a6acb6ed
David Yee <yeed>
2009-06-01 19:08:13 +0000

    Created new LBUtil.java file.
    SVN-Revision: 43

---
+++ software/newtermform/src/java/gov/nih/nci/evs/newtermform/utils/LBUtils.java
@@ -0,0 +1,15 @@
+package gov.nih.nci.evs.newtermform.utils;
+
+import java.util.*;
+
+public class LBUtils {
+    public static List<String> getVocabularyList() {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("BioC_0902D");
+        list.add("CBO2007_06");
+        list.add("CDC_0902D");
+        list.add("CDISC_0902D");
+        list.add("...");
+        return list;
+    }
+}
