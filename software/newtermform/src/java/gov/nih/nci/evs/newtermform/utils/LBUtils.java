23b07d20dccc1a08d9902df4c09da05b27bd5f0a
David Yee <yeed>
2009-06-02 02:08:09 +0000

    Added MODIFIABLE_PROPERTIES enum into LBUtils class.
    SVN-Revision: 46

--- software/newtermform/src/java/gov/nih/nci/evs/newtermform/utils/LBUtils.java
+++ software/newtermform/src/java/gov/nih/nci/evs/newtermform/utils/LBUtils.java
@@ -3,6 +3,25 @@ package gov.nih.nci.evs.newtermform.utils;
 import java.util.*;
 
 public class LBUtils {
+    public static enum MODIFIABLE_PROPERTIES { 
+        DEFINITION("Definition"), SYNONYM("Synonym"), OTHERS("Others");
+        
+        private String _name = "";
+        private void setName(String name) { _name = name; }
+        public String getName() { return _name; }
+        
+        private MODIFIABLE_PROPERTIES(String name){
+            setName(name);
+        }
+        
+        public static List<String> getNameList() {
+            ArrayList<String> list = new ArrayList<String>();
+            for (MODIFIABLE_PROPERTIES item : values())
+                list.add(item.getName());
+            return list;
+        }
+    };
+    
     public static List<String> getVocabularyList() {
         ArrayList<String> list = new ArrayList<String>();
         list.add("BioC_0902D");
