9484a6d00f3e592868990d363da692c5cc7485ba
David Yee <yeed>
2009-06-09 19:02:09 +0000

    Cleaned up Constants.java.
    SVN-Revision: 82

--- software/newtermform/src/java/gov/nih/nci/evs/newtermform/common/Constants.java
+++ software/newtermform/src/java/gov/nih/nci/evs/newtermform/common/Constants.java
@@ -1,18 +1,12 @@
 package gov.nih.nci.evs.newtermform.common;
 
-/**
- * Application constants class
- * @author David Yee
- */
 public interface Constants {
-
     // Application version
-    static public final int MAJOR_VER = 1;
-    static public final int MINOR_VER = 0;
-    static public final String CONFIG_FILE = "NewTermProperties.xml";
+    public static final int MAJOR_VER = 1;
+    public static final int MINOR_VER = 0;
 
     // Application error constants
-    static public final String INIT_PARAM_ERROR_PAGE = "errorPage";
-    static public final String ERROR_MESSAGE = "systemMessage";
-    static public final String ERROR_UNEXPECTED = "Warning: An unexpected processing error has occurred.";  
+    public static final String INIT_PARAM_ERROR_PAGE = "errorPage";
+    public static final String ERROR_MESSAGE = "systemMessage";
+    public static final String ERROR_UNEXPECTED = "Warning: An unexpected processing error has occurred.";
 }
