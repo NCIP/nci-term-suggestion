31220280f7b0aa1445863358f0fbb2237deef986
David Yee <yeed>
2009-06-15 15:00:39 +0000

    Added "_blank" argument to window.open javascript method.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/newterm/trunk@113 be33b9fd-13a8-431c-93df-07cce8752129

--- software/newtermform/web/js/utils.js
+++ software/newtermform/web/js/utils.js
@@ -1,6 +1,6 @@
  function displayLinkInNewWindow(id) {
   var element = document.getElementById(id);
   var url = element.value;
-  element.onclick=window.open(url);
+  element.onclick=window.open(url + "_blank");
  }
  
