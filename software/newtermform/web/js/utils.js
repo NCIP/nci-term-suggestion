2c8ab85b6f3001cc0ba072d09fdafe635eb87905
David Yee <yeed>
2009-06-15 15:11:03 +0000

    Added Firefox specific comment to javascript.displayLinkInNewWindow.
    SVN-Revision: 114

--- software/newtermform/web/js/utils.js
+++ software/newtermform/web/js/utils.js
@@ -1,6 +1,14 @@
- function displayLinkInNewWindow(id) {
+//----------------------------------------------------------------------------//
+// Note: The Firefox browser is initial configure to display a URL
+//   within a separate tab when calling the window.open method.
+//   To configure Firefox display within a separate window:
+//     * Start up Firefox,
+//     * Select Menubar --> Tools --> Options,
+//     * Select "Tabs" tab,
+//     * Select "a new window" radio button.
+//----------------------------------------------------------------------------//
+function displayLinkInNewWindow(id) {
   var element = document.getElementById(id);
   var url = element.value;
   element.onclick=window.open(url + "_blank");
- }
- 
+}
