c0d5b0390d55293395d91462c6fce2fe59464d18
David Yee <yeed>
2009-06-29 16:03:30 +0000

    Defaulted index.jsp page to display change request page instead of new concept page.
    SVN-Revision: 166

--- software/build/build.cmd
+++ software/build/build.cmd
@@ -28,7 +28,8 @@ if "%1" == "clean" (
     goto DONE
 )
 if "%1" == "dev" (
-    ant -Dproperties.file=C:\SVN-Projects\nci-newterm-properties\properties\dev-upgrade.properties deploy:remote:upgrade
+    rem ant -Dproperties.file=C:\SVN-Projects\nci-newterm-properties\properties\dev-upgrade.properties deploy:remote:upgrade
+    ant -Dproperties.file=C:\@\src\browsers\ncit.properties\dev-upgrade.properties deploy:remote:upgrade
     goto DONE
 )
 
