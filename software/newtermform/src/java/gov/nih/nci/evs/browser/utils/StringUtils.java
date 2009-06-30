b822b4767eabe30c8dc3fbc2093efabd4367e074
David Yee <yeed>
2009-06-30 12:36:30 +0000

    Moved StopWatch class out of StringUtils.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/newterm/trunk@181 be33b9fd-13a8-431c-93df-07cce8752129

--- software/newtermform/src/java/gov/nih/nci/evs/browser/utils/StringUtils.java
+++ software/newtermform/src/java/gov/nih/nci/evs/browser/utils/StringUtils.java
@@ -1,40 +1,11 @@
 package gov.nih.nci.evs.browser.utils;
 
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class StringUtils {
     public static final String SEPARATOR = 
         "----------------------------------------"
         + "----------------------------------------";
-    private static DecimalFormat _doubleFormatter = new DecimalFormat("0.00");
-
-    public static class StopWatch {
-        private long _startMS = 0;
-
-        public StopWatch() {
-            start();
-        }
-
-        public void start() {
-            _startMS = System.currentTimeMillis();
-        }
-
-        public long duration() {
-            return System.currentTimeMillis() - _startMS;
-        }
-
-        public String getResult() {
-            long time = duration();
-            double timeSec = time / 1000.0;
-            double timeMin = timeSec / 60.0;
-
-            return "Run time: " + time + " ms, "
-                + _doubleFormatter.format(timeSec) + " sec, "
-                + _doubleFormatter.format(timeMin) + " min";
-        }
-    }
 
     public static String[] toStrings(String value, String delimiter,
         boolean includeDelimiter, boolean trim) {
