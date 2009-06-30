83e127253d65d944b00874e5ca3a1b32fd2ce3bc
David Yee <yeed>
2009-06-30 18:23:21 +0000

    Moved MODIFIABLE_PROPERTY and PROPERTY_ACTION enums from LBUtils to Prop. Created new Prop class.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/newterm/trunk@193 be33b9fd-13a8-431c-93df-07cce8752129

--- software/newtermform/src/java/gov/nih/nci/evs/browser/utils/LBUtils.java
+++ software/newtermform/src/java/gov/nih/nci/evs/browser/utils/LBUtils.java
@@ -3,46 +3,13 @@ package gov.nih.nci.evs.browser.utils;
 import java.util.*;
 
 public class LBUtils {
-    public static enum MODIFIABLE_PROPERTY { 
-        Definition, Synonym, Others;
-        
-        public static MODIFIABLE_PROPERTY valueOfOrDefault(String text) {
-            try {
-                return valueOf(text);
-            } catch (Exception e) {
-                return Definition;
-            }
-        }
-    }
-
-    public static enum PROPERTY_ACTION { 
-        Add, Modify, Delete;
-        
-        public static ArrayList<String> names() {
-            PROPERTY_ACTION[] values = values();
-            ArrayList<String> list = new ArrayList<String>();
-            for (int i=0; i<values.length; ++i) {
-                list.add(values[i].name());
-            }
-            return list;
-        }
-
-        public static PROPERTY_ACTION valueOfOrDefault(String text) {
-            try {
-                return valueOf(text);
-            } catch (Exception e) {
-                return Add;
-            }
-        }
-    }
-    
-    public static String[] getProperties(MODIFIABLE_PROPERTY property) {
+    public static String[] getProperties(Prop.MODIFIABLE_PROPERTY property) {
         ArrayList<String> list = new ArrayList<String>();
-        if (property == MODIFIABLE_PROPERTY.Definition) {
+        if (property == Prop.MODIFIABLE_PROPERTY.Definition) {
             list.add("Definition: A liquid tissue; its major function is to transport oxygen throughout the body. It also supplies the tissues with nutrients, removes waste products, and contains various components of the immune system defending the body against infection. Several hormones also travel in the blood.");
             list.add("CDISC Definition: A liquid tissue with the primary function of transporting oxygen and carbon dioxide. It supplies the tissues with nutrients, removes waste products, and contains various components of the immune system defending the body against infection.");
             list.add("NCI-GLOSS Definition: Blood circulating throughout the body.");
-        } else if (property == MODIFIABLE_PROPERTY.Synonym) {
+        } else if (property == Prop.MODIFIABLE_PROPERTY.Synonym) {
             list.add("Term: Blood; Source: CTRM; Type: DN");      
             list.add("Term: BLOOD; Source: CDISC; Type: PT");  
             list.add("Term: Blood; Source: NCI; Type: PT");  
@@ -56,7 +23,7 @@ public class LBUtils {
         return list.toArray(new String[list.size()]);
     }
     
-    public static String getProperty(MODIFIABLE_PROPERTY property, int index) {
+    public static String getProperty(Prop.MODIFIABLE_PROPERTY property, int index) {
         String[] list = getProperties(property);
         if (index >= 0 && index < list.length)
             return list[index];
