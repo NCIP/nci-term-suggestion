d5c8847f539ca7e8fff5f69f594c06964573ccc9
David Yee <yeed>
2009-07-05 14:07:10 +0000

    Added caDSR comboBox into suggestion form.
    SVN-Revision: 224

--- software/newtermform/src/java/gov/nih/nci/evs/browser/bean/Prop.java
+++ software/newtermform/src/java/gov/nih/nci/evs/browser/bean/Prop.java
@@ -16,13 +16,13 @@ public class Prop {
         }
     }
     
-    public static String[] getProperties(Prop.Modifiable property) {
+    public static String[] getProperties(Modifiable property) {
         ArrayList<String> list = new ArrayList<String>();
-        if (property == Prop.Modifiable.Definition) {
+        if (property == Modifiable.Definition) {
             list.add("Definition: A liquid tissue; its major function is to transport oxygen throughout the body. It also supplies the tissues with nutrients, removes waste products, and contains various components of the immune system defending the body against infection. Several hormones also travel in the blood.");
             list.add("CDISC Definition: A liquid tissue with the primary function of transporting oxygen and carbon dioxide. It supplies the tissues with nutrients, removes waste products, and contains various components of the immune system defending the body against infection.");
             list.add("NCI-GLOSS Definition: Blood circulating throughout the body.");
-        } else if (property == Prop.Modifiable.Synonym) {
+        } else if (property == Modifiable.Synonym) {
             list.add("Term: Blood; Source: CTRM; Type: DN");      
             list.add("Term: BLOOD; Source: CDISC; Type: PT");  
             list.add("Term: Blood; Source: NCI; Type: PT");  
@@ -36,7 +36,7 @@ public class Prop {
         return list.toArray(new String[list.size()]);
     }
     
-    public static String getProperty(Prop.Modifiable property, int index) {
+    public static String getProperty(Modifiable property, int index) {
         String[] list = getProperties(property);
         if (index >= 0 && index < list.length)
             return list[index];
@@ -61,6 +61,18 @@ public class Prop {
                 return valueOf(text);
             } catch (Exception e) {
                 return Add;
+            }
+        }
+    }
+
+    //-- Enum CADSR ------------------------------------------------------------
+    public static enum CADSR {
+        None, CADSR_OC, CADSR_PROP, CADSR_REP, CADSR_VV;
+        public static CADSR valueOfOrDefault(String text) {
+            try {
+                return valueOf(text);
+            } catch (Exception e) {
+                return None;
             }
         }
     }
