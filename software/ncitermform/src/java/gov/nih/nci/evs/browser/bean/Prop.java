9252e57b4a9932be7a30a84c1a8e6790037756a4
David Yee <yeed>
2009-07-07 01:03:50 +0000

    Suggestion page is now using the configurable list for sources.
    SVN-Revision: 243

--- software/ncitermform/src/java/gov/nih/nci/evs/browser/bean/Prop.java
+++ software/ncitermform/src/java/gov/nih/nci/evs/browser/bean/Prop.java
@@ -140,28 +140,4 @@ public class Prop {
             }
         }
     }
-    
-    public static enum Source {
-        BioC_0902D, CBO2007_06, CDC_0902D, CDISC_0902D, COH_0902D,
-        CRCH_0902D, CTCAE_0902D, CTEP04, DCP_0902D, DICOM_0902D,
-        DTP_0902D, ELC2001, FDA_0902D, ICD03, ICH_0902D, JAX_0902D,
-        KEGG_0902D, MDBCAC2005_12, MDR10_0, MGED131, 
-        NCI_GLOSS_0902D /* NCI-GLOSS_0902D */, 
-        NCI_HL7_0902D /* NCI-HL7_0902D */, NCI2009_02D, PDQ2009_02,
-        PMA2007, RADLEX2_01, RENI_0902D, UCUM_0902D, ZFIN_0902D;
-        
-        public static Source valueOfOrDefault(String text) {
-            try {
-                String textStr = text.toLowerCase();
-                for (Source value : values()) {
-                    String valueStr = value.name().toLowerCase();
-                    if (valueStr.equals(textStr))
-                        return value;
-                }
-                return values()[0];
-            } catch (Exception e) {
-                return values()[0];
-            }
-        }
-    }
 }
