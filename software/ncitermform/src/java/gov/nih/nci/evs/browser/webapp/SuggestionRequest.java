918471c8171b23e839ca30dbd50d43c6f547548b
David Yee <yeed>
2009-07-07 16:17:58 +0000

    Moved Prop from bean packate to webapp.
    SVN-Revision: 259

--- software/ncitermform/src/java/gov/nih/nci/evs/browser/webapp/SuggestionRequest.java
+++ software/ncitermform/src/java/gov/nih/nci/evs/browser/webapp/SuggestionRequest.java
@@ -1,7 +1,5 @@
 package gov.nih.nci.evs.browser.webapp;
 
-import gov.nih.nci.evs.browser.bean.Prop;
-import gov.nih.nci.evs.browser.bean.Prop.Version;
 import gov.nih.nci.evs.browser.properties.*;
 import gov.nih.nci.evs.browser.utils.*;
 
