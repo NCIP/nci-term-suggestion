acaf04f6f0b9f3c35e913c417f756e3dc481a248
David Yee <yeed>
2009-07-09 21:01:33 +0000

    GF22273: First pass at handling wrapping of confirmation message.
    SVN-Revision: 302

--- software/ncitermform/src/java/gov/nih/nci/evs/browser/utils/StringUtils.java
+++ software/ncitermform/src/java/gov/nih/nci/evs/browser/utils/StringUtils.java
@@ -42,4 +42,48 @@ public class StringUtils {
         }
         return buffer.toString();
     }
+
+    public static String wrap(int maxCharInLine, String text) {
+        StringTokenizer tokenizer = new StringTokenizer(text, "\n", true);
+        StringBuffer buffer = new StringBuffer();
+        while (tokenizer.hasMoreTokens()) {
+            String line = tokenizer.nextToken();
+            buffer.append(wrapOneLine(maxCharInLine, "\n", line));
+        }
+        return buffer.toString();
+    }
+
+    public static int indexOfLastWhiteSpace(String text) {
+        for (int i = text.length() - 1; i >= 0; --i) {
+            char c = text.charAt(i);
+            if (Character.isWhitespace(c))
+                return i;
+        }
+        return -1;
+    }
+
+    public static String wrapOneLine(int maxCharInLine, String endOfLine,
+            String text) {
+        StringBuffer buffer = new StringBuffer();
+        do {
+            int start = 0, n = text.length();
+            int end = start + maxCharInLine;
+            if (end >= n)
+                end = n;
+
+            if (end < n && !Character.isWhitespace(text.charAt(end))) {
+                String line = text.substring(start, end);
+                int i = indexOfLastWhiteSpace(line);
+                if (i > 0)
+                    end = i;
+            }
+
+            String line = text.substring(start, end);
+            if (buffer.length() > 0)
+                buffer.append(endOfLine);
+            buffer.append(line);
+            text = text.substring(end).trim();
+        } while (text.length() > 0);
+        return buffer.toString();
+    }
 }
