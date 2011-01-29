d08c65cfa23c6ffca7631685e397beecec4c024c
Will Garcia <garciawa2>
2011-01-29 17:33:33 +0000

    Upgrade to BDA-Lite.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/newterm/trunk@481 be33b9fd-13a8-431c-93df-07cce8752129

--- software/build/build.cmd
+++ software/build/build.cmd
@@ -4,15 +4,11 @@
 @rem ******************************************
 setlocal
 @rem Environment settings here...
-set DEVPROPFILE=C:\NCI-Projects\nci-termform-properties\properties\dev-upgrade.properties
-set CIPROPFILE=C:\NCI-Projects\nci-termform-properties\properties\ci-upgrade.properties
-set QAPROPFILE=C:\NCI-Projects\nci-termform-properties\properties\qa-upgrade.properties
-set DATAQAPROPFILE=C:\NCI-Projects\nci-termform-properties\properties\data-qa-upgrade.properties
 set DEBUG=-Denable.install.debug=false
 set TAG=-Danthill.build.tag_built=desktop
 @rem Test is debug has been set
 if "%2" == "debug" (
-    set DEBUG=-Denable.install.debug=true -debug
+    set DEBUG=-debug
 )
 cls
 if "%1" == "" (
@@ -21,47 +17,47 @@ if "%1" == "" (
     echo.
     echo   clean        -- Remove classes directory for clean build
     echo   all          -- Normal build of application
-    echo   install      -- Builds, installs JBoss locally
     echo   upgrade      -- Build and upgrade application
-    echo   dev          -- Builds, upgrades JBoss on DEV
-    echo   ci           -- Builds, upgrades JBoss on CI
-    echo   qa           -- Builds, upgrades JBoss on QA
-    echo   data-qa      -- Builds, upgrades JBoss on Data QA
+    echo   install      -- Builds, installs JBoss locally
+    echo   uninstall    -- Uninstall the web application
     echo   deploy       -- Hot deploy application
     echo   jsp          -- Hot deploy JSP files
-    echo   stop         -- Stop war file
-    echo   start        -- Start war file
-    echo   cissh        -- Test SSH login in CI
+    echo   config       -- Generates configuration files for a target environment
+    echo   stop         -- Shutdown JBoss
+    echo   start        -- Start JBoss
     goto DONE
 )
+
 if "%1" == "all" (
     ant %TAG% build:all
     goto DONE
 )
+
 if "%1" == "upgrade" (
     ant %TAG% %DEBUG% deploy:local:upgrade
     goto DONE
 )
+
 if "%1" == "install" (
     ant %TAG% %DEBUG% deploy:local:install
     goto DONE
 )
+
+if "%1" == "uninstall" (
+    ant %TAG% %DEBUG% deploy:local:uninstall
+    goto DONE
+)
+
 if "%1" == "deploy" (
     ant %TAG% %DEBUG% deploy:hot
     goto DONE
 )
-if "%1" == "stop" (
-    ant %TAG% %DEBUG% deploy:stop
-    goto DONE
-)
-if "%1" == "start" (
-    ant %TAG% %DEBUG% deploy:start
-    goto DONE
-)
+
 if "%1" == "jsp" (
     ant %DEBUG% deploy:hot:jsp
     goto DONE
 )
+
 if "%1" == "clean" (
     ant clean
     if exist ..\target\*.* (
@@ -69,25 +65,21 @@ if "%1" == "clean" (
     )
     goto DONE
 )
-if "%1" == "dev" (
-    ant -Dproperties.file=%DEVPROPFILE% %TAG% %DEBUG% deploy:remote:upgrade
+
+if "%1" == "config" (
+    ant %DEBUG% build:config
     goto DONE
 )
-if "%1" == "ci" (
-    ant -Dproperties.file=%CIPROPFILE% %TAG% %DEBUG% deploy:remote:upgrade
+
+if "%1" == "stop" (
+    ant %DEBUG% jboss:stop
     goto DONE
 )
-if "%1" == "qa" (
-    ant -Dproperties.file=%QAPROPFILE% %TAG% %DEBUG% deploy:remote:upgrade
+
+if "%1" == "start" (
+    ant %DEBUG% jboss:start
     goto DONE
 )
-if "%1" == "data-qa" (
-    ant -Dproperties.file=%DATAQAPROPFILE% %TAG% %DEBUG% deploy:remote:upgrade
-    goto DONE
-)
-if "%1" == "cissh" (
-    ssh jboss51a@ncias-c512-v.nci.nih.gov -i C:\NCI-Projects\ncit-properties\properties\ssh-keys\id_dsa_bda echo "Test worked!"
-    goto DONE
-)
+
 :DONE
 endlocal
